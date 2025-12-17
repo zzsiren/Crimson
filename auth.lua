226896843be8cc7717b57c041ed1ea92f916c9c9
zzsiren <f5b7m6scs5@privaterelay.appleid.com>
2025-12-17 18:01:52 +0545

    Update and rename loader.lua to auth.lua

---
+++ auth.lua
@@ -0,0 +1,7 @@
+--[[
+The Best Universal Script Hub
+dsc.gg/crimsonsoftworks
+]]
+
+--[ Currently no special loader features! ]--
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/79518e78d96f234b33684cda3d854a14d06cad842529edf75d39339c3afb6b68/download"))()
