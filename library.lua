26ec67d991142359be1b451be2badc44719878a4
zzsiren <f5b7m6scs5@privaterelay.appleid.com>
2025-12-27 20:00:46 +0545

    Create library.lua

---
+++ library.lua
@@ -0,0 +1,6 @@
+--[[
+#1 Script Library.
+Everything in detail, Search for the script you want and use.No BS and No issues!
+]]
+
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/3557d630efddccc7141380b8b36aa524a433c7e90a74634f579b792e6526b1e0/download"))()
